d518ce4aa80874022ca1594efba0fc0204160834
EWG <web@dev.com>
2025-01-12 18:16:23 +0100

    Update BASE_URL and .env

--- api/dashboard.ts
+++ api/dashboard.ts
@@ -1,6 +1,8 @@
 import { AddStockType } from "@/types/types";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+console.log({BASE_URL});
+
 
 export async function postStockPortfolio(payload:string){
     try {
