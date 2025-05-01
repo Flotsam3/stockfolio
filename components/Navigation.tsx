bc93575302c23533c87df35e0bdbc14902c7ae5e
flotsam <mpb1@e.mail.de>
2025-05-01 15:05:03 +0200

    fix(Navigation): typo in fundamentals

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
       <nav className='hidden md:block fixed top-0 w-full z-10'>
           <ul className='flex bg-main-dark text-white py-7 justify-around'>
               <li className='hover:text-slate-400'><Link href="/">Dashboard</Link></li>
-              <li className='hover:text-slate-400'><Link href="/fundamentals">Fundamentials</Link></li>
+              <li className='hover:text-slate-400'><Link href="/fundamentals">Fundamentals</Link></li>
               <li className='hover:text-slate-400'><Link href="/exchanges">Exchanges</Link></li>
           </ul>
       </nav>
@@ -22,7 +22,7 @@ export default function Navigation() {
         <i onClick={()=>setShowMenu(!showMenu)} className="fa-solid fa-bars cursor-pointer" style={{color:"#fff"}}></i>
         <ul className={`absolute flex flex-col justify-start gap-4 right-0 h-[100vh] ${showMenu ? "translate-x-0" : "translate-x-80"} top-14 bg-main-dark text-white py-7  text-right px-7 transition ease-in-out duration-1000`}>
             <li className='hover:text-slate-400'><Link onClick={()=>setShowMenu(false)} href="/">Dashboard</Link></li>
-            <li className='hover:text-slate-400'><Link onClick={()=>setShowMenu(false)} href="/fundamentals">Fundamentials</Link></li>
+            <li className='hover:text-slate-400'><Link onClick={()=>setShowMenu(false)} href="/fundamentals">Fundamentals</Link></li>
             <li className='hover:text-slate-400'><Link onClick={()=>setShowMenu(false)} href="/exchanges">Exchanges</Link></li>
             <li className='hover:text-slate-400'><Link onClick={()=>{setShowMenu(false); setShowAddWatchlist(true)}} href="/">Add Stock</Link></li>
         </ul>
